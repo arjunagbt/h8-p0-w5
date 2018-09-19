38d94ff8924d18bc942445c8d1708faf70bd85aa
arjunagbt <arjunagbt@gmail.com>
2018-09-19 11:41:27 +0700

    submit

---
+++ exercise-8.js
@@ -0,0 +1,17 @@
+function cariPelaku(str) {
+    // you can only write your code here!
+
+    strMod = str.match(/abc{1}/g, '')
+
+    return strMod.length
+
+  }
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
+
+  //Arjuna Sarumpaet
